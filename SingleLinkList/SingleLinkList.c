7485a338e9cd8f0982079e69a7577d1b85390336
jdesai <jdesai@airspan.com>
2022-01-02 13:57:22 +0530

    git

--- SingleLinkList/SingleLinkList.c
+++ SingleLinkList/SingleLinkList.c
@@ -1,1 +1,110 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+// Include header file of SingleLinkList
+#include "SingleLinkList.h"
+
+void main()
+{
+    SINGLE_LINK * headPtr = NULL;
+    
+    add_single_link_at_last(&headPtr, 10);
+    add_single_link_at_last(&headPtr, 20);
+    add_single_link_at_last(&headPtr, 30);
+    add_single_link_at_last(&headPtr, 40);
+
+    print_single_link_list(headPtr);
+
+    delete_single_link_list(&headPtr);
+
+    print_single_link_list(headPtr);
+
+}
+
+SINGLE_LINK * create_new_node (int data)
+{
+    SINGLE_LINK * newNodePtr = (SINGLE_LINK *) calloc(1, sizeof(SINGLE_LINK));
+    if(newNodePtr) {
+        newNodePtr->data = data;
+        newNodePtr->next = NULL;
+    }
+    return newNodePtr;
+}
+
+bool add_single_link_at_last (SINGLE_LINK ** headPtr, int data)
+{
+    bool ret_val = FAIL;
+    if (headPtr) {
+
+        SINGLE_LINK * newNodePtr = create_new_node(data);
+        if (!newNodePtr) {
+            printf(" Memory allocation fail \n");
+            return FAIL;
+        }
+        
+        if (NULL == *headPtr) {
+            *headPtr = newNodePtr;
+        }
+        else {
+            SINGLE_LINK * currentNodePtr = *headPtr;
+            while(currentNodePtr->next)
+                currentNodePtr = currentNodePtr->next;
+            currentNodePtr->next = newNodePtr;
+        }
+        return SUCCESS;
+    }
+    else {
+        return FAIL;
+    }
+}
+
+void print_single_link_with_data (SINGLE_LINK * headPtr)
+{
+    SINGLE_LINK * currentNodePtr = headPtr;
+    printf(" Single Link List (data) : ");
+    while(currentNodePtr)
+    {
+        printf("%d-->", currentNodePtr->data);
+        currentNodePtr = currentNodePtr->next;
+    }
+    printf("NULL\n");
+    print_single_link_with_addr(headPtr);
+}
+
+void print_single_link_with_addr (SINGLE_LINK * headPtr)
+{
+    SINGLE_LINK * currentNodePtr = headPtr;
+    printf(" Single Link List (Addr) : ");
+    while(currentNodePtr)
+    {
+        printf("%.3p-->", (void *) currentNodePtr);
+        currentNodePtr = currentNodePtr->next;
+    }
+    printf("NULL\n");
+}
+
+void print_single_link_list (SINGLE_LINK * headPtr)
+{
+    SINGLE_LINK * currentNodePtr = headPtr;
+    print_single_link_with_data(currentNodePtr);
+}
+
+bool delete_single_link_list (SINGLE_LINK ** headPtr)
+{
+    if(*headPtr) {
+        SINGLE_LINK * currentNodePtr;
+        while(*headPtr)
+        {
+            currentNodePtr = *headPtr;
+            (*headPtr) = (*headPtr)->next;
+            free(currentNodePtr);
+            currentNodePtr = NULL;
+        }
+        return SUCCESS;
+    }
+    else {
+        printf(" HeadPtr is empty\n");
+        return FAIL;
+    }
+}
---
+++ SingleLinkList/SingleLinkList.h
@@ -0,0 +1,44 @@
+
+#ifndef _SINGLE_LINK_LIST_
+#define _SINGLE_LINK_LIST_
+
+    #define SUCCESS 1
+    #define FAIL    2
+
+    typedef unsigned char bool;
+
+    typedef struct _single_link_list_
+    {
+        int data;
+        struct _single_link_list_ * next;
+
+    }SINGLE_LINK;
+
+    typedef enum _arrange_order_
+    {
+        ARRANGE_IN_ASCENDING    = 0,
+        ARRANGE_IN_DESCENDING,  //1
+
+    }ARRANGE_ORDER;
+
+    SINGLE_LINK * create_new_node       (int data);
+
+    bool add_single_link_at_last        (SINGLE_LINK ** headPtr, int data);
+    bool add_single_link_at_first       (SINGLE_LINK ** headPtr, int data);
+    bool add_single_link_at_pos         (SINGLE_LINK ** headPtr, int data);
+    bool add_single_link_with_ascending (SINGLE_LINK ** heardPtr, int data);
+    
+    void print_single_link_with_data    (SINGLE_LINK * headPtr);
+    void print_single_link_with_addr    (SINGLE_LINK * headPtr);
+    void print_single_link_list         (SINGLE_LINK * headPtr);
+
+    bool arrange_in_order           (ARRANGE_ORDER order, SINGLE_LINK ** headPtr);
+    bool swap_with_data             (SINGLE_LINK * singlePtr1, SINGLE_LINK * singlePtr2);
+    bool swap_with_address          ();
+    
+    bool delete_single_node         (SINGLE_LINK ** headPtr, int data);
+    bool delete_single_node_r       (SINGLE_LINK ** headPtr, int data);     // _r Indicate recursion
+    bool delete_single_link_list    (SINGLE_LINK ** headPtr);
+    bool delete_single_link_list_r  (SINGLE_LINK ** headPtr);               // _r Indicate recursion
+
+#endif  // _SINGLE_LINK_LIST_
