36d8ac8959b10f096b8c9e929bd6b719e3f459bc
Jay Desai <50072546+jjdesai@users.noreply.github.com>
2022-01-26 01:26:22 +0530

    Added Few API in General Tree

--- Tree/General/GeneralTree.h
+++ Tree/General/GeneralTree.h
@@ -32,19 +32,22 @@
     void print_in_preorder  (GENERAL_TREE * headPtr);
     void print_in_inorder   (GENERAL_TREE * headPtr);
     void print_in_postorder (GENERAL_TREE * headPtr);
+    void print_in_list      (GENERAL_TREE * headPtr);
 
     bool delete_general_tree            (GENERAL_TREE ** headPtr);
     bool delete_node_in_general_tree    (GENERAL_TREE ** headPtr, int data);
 
-    int find_root   (GENERAL_TREE * headPtr);
-    bool is_sibling (GENERAL_TREE * headPtr, int data1, int data2);
+    int get_root_node           (GENERAL_TREE * headPtr);
+    bool is_sibling             (GENERAL_TREE * headPtr, int data1, int data2);
+    bool print_siblings         (GENERAL_TREE * headPtr, int data);
     void print_internal_node    (GENERAL_TREE * headPtr);
+    int get_internet_node_count (GENERAL_TREE * headPtr);
     void print_leaf_node        (GENERAL_TREE * headPtr);   // Same --> void print_external_node    (GENERAL_TREE * headPtr);
-
-    
-    
-
-
-
+    int get_breadth             (GENERAL_TREE * headPtr);   // The number of leaves
+    bool print_ancestor         (GENERAL_TREE * headPtr, int data);
+    bool print_escendants       (GENERAL_TREE * headPtr, int data);
+    int get_depth               (GENERAL_TREE * headPtr);
+    int get_height              (GENERAL_TREE * headPtr);
+    int total_node_at_level     (GENERAL_TREE * headPtr, int level);
 
 #endif // _GENERAL_TREE_H_
