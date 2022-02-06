d41536b1c17d4e81d4fe8ec5469fdb54c861cb61
jjdesai <jaydesai36@gmail.com>
2022-02-06 14:59:38 +0530

    Added Data structures Terminologies

---
+++ Tree/Binary/Balanced_BinaryTree/Balanced_BinaryTree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "../PrintMyBinaryTree.c"
+#include "../BinaryCommon.c"    // As Make File is not completed, need to add .c
+                                // Chnage it to .h once makefile is ready
+#include "Skewed_BinaryTree.h"
+
+int main (int argc, char ** argv)
+{
+    
+
+    return 1;
+}
