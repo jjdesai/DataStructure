14e40be9f4891b9c24eab955f30648ddbbf2c401
jjdesai <jaydesai36@gmail.com>
2022-03-01 23:23:33 +0530

    Basic Changes

--- Tree/Binary/Perfect_BinaryTree/Perfect_BinaryTree.c
+++ Tree/Binary/Perfect_BinaryTree/Perfect_BinaryTree.c
@@ -19,59 +19,73 @@
 
 int main(int argc, char ** argv)
 {
-    BINARY_TREE * headPtr = NULL, * headPtr1 = NULL;
+    BINARY_TREE * rootPtr = NULL, * rootPtr1 = NULL;
 
-    headPtr = create_binary_tree(10);
+    rootPtr = create_binary_tree(10);
 
-    headPtr->left = create_binary_tree(20);
-    headPtr->right = create_binary_tree(30);
+    rootPtr->left = create_binary_tree(20);
+    rootPtr->right = create_binary_tree(30);
 
-    headPtr->left->left = create_binary_tree(40);
-    headPtr->left->right = create_binary_tree(50);
-    headPtr->right->left = create_binary_tree(60);
-    headPtr->right->right = create_binary_tree(70);
+    rootPtr->left->left = create_binary_tree(40);
+    rootPtr->left->right = create_binary_tree(50);
+    rootPtr->right->left = create_binary_tree(60);
+    rootPtr->right->right = create_binary_tree(70);
 
-    headPtr->left->left->left = create_binary_tree(80);
-    headPtr->left->left->right = create_binary_tree(90);
-    headPtr->left->right->left = create_binary_tree(100);
-    headPtr->left->right->right = create_binary_tree(110);
-    headPtr->right->left->left = create_binary_tree(120);
-    headPtr->right->left->right = create_binary_tree(130);
-    headPtr->right->right->left = create_binary_tree(140);
-    headPtr->right->right->right = create_binary_tree(150);
+    rootPtr->left->left->left = create_binary_tree(80);
+    rootPtr->left->left->right = create_binary_tree(90);
+    rootPtr->left->right->left = create_binary_tree(100);
+    rootPtr->left->right->right = create_binary_tree(110);
+    rootPtr->right->left->left = create_binary_tree(120);
+    rootPtr->right->left->right = create_binary_tree(130);
+    rootPtr->right->right->left = create_binary_tree(140);
+    rootPtr->right->right->right = create_binary_tree(150);
 
-    lowest_common_ancestor_in_binary_tree(headPtr, 50, 90);
-    lowest_common_ancestor_in_binary_tree(headPtr, 50, 100);
+    lowest_common_ancestor_in_binary_tree(rootPtr, 50, 90);
+    lowest_common_ancestor_in_binary_tree(rootPtr, 50, 100);
 
-    print_my_binary_tree(headPtr);
+    print_my_binary_tree(rootPtr);
 
-    printf(" Total no of Node : [%lu]\n", total_node_in_perfect_binary_tree(headPtr));
+    printf(" Total no of Node : [%lu]\n", total_node_in_perfect_binary_tree(rootPtr));
 
-    if(is_tree_a_perfect_binary_tree(headPtr))
+    if(is_tree_a_perfect_binary_tree(rootPtr))
         printf(" Given Binary Tree is a Perfect Binary Tree\n");
     else
         printf(" Given Binary Tree is not a Perfect Binary Tree\n");
 
-    delete_binary_tree(&headPtr);
+    delete_binary_tree(&rootPtr);
 
-    printf(" Total no of Node : [%lu]\n", total_node_in_perfect_binary_tree(headPtr));
+    printf(" Total no of Node : [%lu]\n", total_node_in_perfect_binary_tree(rootPtr));
 }
 
-unsigned int get_height_of_perfect_binary_tree (BINARY_TREE * headPtr)
+/*
+    Author : Jay Desai      Tester : Jay Desai
+    Status : Working
+    Description : Return the height Perfect Binary Tree
+    Parameter : rootPtr (In) -> Head Pointer 
+    Return :    Return the height of Perfect Binary Tree
+*/
+unsigned int get_height_of_perfect_binary_tree (BINARY_TREE * rootPtr)
 {
-    if(is_binary_tree_empty(headPtr))
+    if(is_binary_tree_empty(rootPtr))
         return 0;
     else
-        return (1 + get_height_of_perfect_binary_tree(headPtr->left));
+        return (1 + get_height_of_perfect_binary_tree(rootPtr->left));
 }
 
-long unsigned int total_node_in_perfect_binary_tree (BINARY_TREE * headPtr)
+/*
+    Author : Jay Desai      Tester : Jay Desai
+    Status : Working
+    Description : Count the total number of nodes in Perfect Binary Tree
+    Parameter : rootPtr (In) -> Head Pointer 
+    Return :    Return the number of nodes present in Perfect Binary Tree
+*/
+long unsigned int total_node_in_perfect_binary_tree (BINARY_TREE * rootPtr)
 {
-    if(is_binary_tree_empty(headPtr))
+    if(is_binary_tree_empty(rootPtr))
         return 0;
     else
     {
-        unsigned int height = get_height_of_perfect_binary_tree(headPtr);
+        unsigned int height = get_height_of_perfect_binary_tree(rootPtr);
         long unsigned int totalNode = 1;
         for(int i=0; i<height; i++)
             totalNode *= 2;
@@ -79,14 +93,22 @@ long unsigned int total_node_in_perfect_binary_tree (BINARY_TREE * headPtr)
     }
 }
 
-int is_tree_a_perfect_binary_tree (BINARY_TREE * headPtr)
+/*
+    Author : Jay Desai      Tester : Jay Desai
+    Status : Working
+    Description : Will check whether tree is Perfect Binary Tree or not
+    Parameter : rootPtr (In) -> Head Pointer 
+    Return :    0 --> If Tree is not Perfect Binary Tree
+                1 --> If Tree is Perfect Binary Tree
+*/
+int is_tree_a_perfect_binary_tree (BINARY_TREE * rootPtr)
 {
     unsigned int treeSize = 0;
     unsigned int totalNodeFromHeight = 0;
     
-    totalNodeFromHeight = total_node_in_perfect_binary_tree(headPtr);
+    totalNodeFromHeight = total_node_in_perfect_binary_tree(rootPtr);
     
-    treeSize = total_node_in_binary_tree(headPtr);
+    treeSize = total_node_in_binary_tree(rootPtr);
 
     return (totalNodeFromHeight == treeSize);
 }
